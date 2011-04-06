559e4f2ef80b291f781939f15695562ec559939b
Irakli Gozalishvili <rfobic@gmail.com>
2011-04-06 12:53:43 -0700

    Rename luncher.js -> launcher.js

--- data/launcher.js
+++ data/launcher.js
@@ -1,18 +1,30 @@
 define(function(require, exports, module) {
 
-exports.launch = function(env) {
+exports.startup = function(data) {
+    var env = data.env;
+
     var event = require("pilot/event");
     var Editor = require("ace/editor").Editor;
     var Renderer = require("ace/virtual_renderer").VirtualRenderer;
     var theme = require("ace/theme/twilight");
     var EditSession = require("ace/edit_session").EditSession;
+
     var JavaScriptMode = require("ace/mode/javascript").Mode;
     var CssMode = require("ace/mode/css").Mode;
     var HtmlMode = require("ace/mode/html").Mode;
     var XmlMode = require("ace/mode/xml").Mode;
     var PythonMode = require("ace/mode/python").Mode;
     var PhpMode = require("ace/mode/php").Mode;
+    var JavaMode = require("ace/mode/java").Mode;
+    var CSharpMode = require("ace/mode/csharp").Mode;
+    var RubyMode = require("ace/mode/ruby").Mode;
+    var CCPPMode = require("ace/mode/c_cpp").Mode;
+    var CoffeeMode = require("ace/mode/coffee").Mode;
+    var PerlMode = require("ace/mode/perl").Mode;
+    var SvgMode = require("ace/mode/svg").Mode;
+    var TextileMode = require("ace/mode/textile").Mode;
     var TextMode = require("ace/mode/text").Mode;
+
     var UndoManager = require("ace/undomanager").UndoManager;
 
     var modes = {
@@ -30,8 +42,12 @@ exports.launch = function(env) {
 
     var container = document.getElementById("editor");
     env.editor = new Editor(new Renderer(container, theme));
-    env.editor.setSelectionStyle("text"); // "line"
-    env.editor.setSession(session)
+    // Each editor should contain reference to an `env` which will be used be
+    // passed to the commands executed by keybindings.
+    env.editor.env = Object.create(env);
+
+    //env.editor.setSelectionStyle("text"); // "line"
+    //env.editor.setSession(session)
 
     function onResize() {
       container.style.width = (document.documentElement.clientWidth) + "px";
