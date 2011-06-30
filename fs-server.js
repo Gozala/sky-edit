0e5f540126dc6dc07695e48fa9ec8263bcb4f4bc
Irakli Gozalishvili <rfobic@gmail.com>
2011-06-30 16:32:19 +0200

    Serialize errors so that they can be passed across the message pipe.

--- fs-server.js
+++ fs-server.js
@@ -1,14 +1,18 @@
 var fs = require("https!raw.github.com/Gozala/jetpack-io/v0.1.0/fs.js");
 
+function serializeError(error) {
+  return error ? { message: error.message } : error;
+}
+
 exports.readFile = function readFile(path, callback) {
   fs.readFile(path, function(error, data) {
-    callback(error, String(data));
+    callback(serializeError(error), String(data));
   });
 };
 
 exports.writeFile = function(path, content, callback) {
   fs.writeFile(path, content, function(error) {
-    callback(error);
+    callback(serializeError(error));
   });
 };
 exports.readURI = function readURI(uri, callback) {
