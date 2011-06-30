e2d95f3e4e56c033be645c675a1a5e69fb869b4a
Irakli Gozalishvili <rfobic@gmail.com>
2011-06-30 18:53:47 +0200

    Synchronizing with changes in the ace.

--- data/launcher.js
+++ data/launcher.js
@@ -4,9 +4,12 @@ exports.startup = function(data) {
     var env = data.env;
 
     var event = require("pilot/event");
+
+    var Range = require("ace/range").Range;
     var Editor = require("ace/editor").Editor;
     var Renderer = require("ace/virtual_renderer").VirtualRenderer;
-    var theme = require("ace/theme/twilight");
+
+    var theme = require("./theme");
     var EditSession = require("ace/edit_session").EditSession;
 
     var JavaScriptMode = require("ace/mode/javascript").Mode;
@@ -20,23 +23,39 @@ exports.startup = function(data) {
     var RubyMode = require("ace/mode/ruby").Mode;
     var CCPPMode = require("ace/mode/c_cpp").Mode;
     var CoffeeMode = require("ace/mode/coffee").Mode;
+    var JsonMode = require("ace/mode/json").Mode;
     var PerlMode = require("ace/mode/perl").Mode;
+    var ClojureMode = require("ace/mode/clojure").Mode;
+    var OcamlMode = require("ace/mode/ocaml").Mode;
     var SvgMode = require("ace/mode/svg").Mode;
     var TextileMode = require("ace/mode/textile").Mode;
     var TextMode = require("ace/mode/text").Mode;
+    var GroovyMode = require("ace/mode/groovy").Mode;
+    var ScalaMode = require("ace/mode/scala").Mode;
 
     var UndoManager = require("ace/undomanager").UndoManager;
 
-    var fs = require('fs');
-
     var modes = env.modes = {
-      text: new TextMode(),
-      xml: new XmlMode(),
-      html: new HtmlMode(),
-      css: new CssMode(),
-      javascript: new JavaScriptMode(),
-      python: new PythonMode(),
-      php: new PhpMode()
+      'javascript': new JavaScriptMode(),
+      'css': new CssMode(),
+      'html': new HtmlMode(),
+      'xml': new XmlMode(),
+      'python': new PythonMode(),
+      'php': new PhpMode(),
+      'java': new JavaMode(),
+      'csharp': new CSharpMode(),
+      'ruby': new RubyMode(),
+      'c_cpp': new CCPPMode(),
+      'coffee': new CoffeeMode(),
+      'json': new JsonMode(),
+      'perl': new PerlMode(),
+      'clojure': new ClojureMode(),
+      'ocaml': new OcamlMode(),
+      'svg': new SvgMode(),
+      'textile': new TextileMode(),
+      'text': new TextMode(),
+      'groovy': new GroovyMode(),
+      'scala': new ScalaMode()
     };
 
     var session = new EditSession('');
