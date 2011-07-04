b776abcc348552215c9547e38061e01929cd26c9
Irakli Gozalishvili <rfobic@gmail.com>
2011-07-04 15:09:05 +0200

    Removing fs-server in favor of direct 'fs' module.

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
-'use strict'
+/* vim:set ts=2 sw=2 sts=2 expandtab */
+/*jshint asi: true undef: true es5: true node: true devel: true
+         forin: true latedef: false browser: true */
+/*global define: true port: true */
+!define(function(require, exports) {
 
-const { data } = require("self")
-  ,   { PageMod } = require("addon-kit/page-mod")
-  ,   protocol = require("https://raw.github.com/Gozala/jetpack-protocol/v0.1.0/protocol.js")
+"use strict";
+
+var data = require("self").data
+var PageMod = require("addon-kit/page-mod").PageMod
+var protocol = require("https://raw.github.com/Gozala/jetpack-protocol/v0.1.0/protocol.js")
 
 const PROTOCOL = 'edit'
 const editorURI = data.url('index.html')
 const rootURI = editorURI.substr(0, editorURI.lastIndexOf('/') + 1)
-const fs = require("./fs-server");
+var fs = require("https://raw.github.com/Gozala/jetpack-io/v0.2.0/fs.js")
+
+function errorToJSON(error) {
+  return error ? { message: error.message, stack: error.stack } : error
+}
 
 var mod  = PageMod({
   include: PROTOCOL + ':*',
   contentScript: 'unsafeWindow.port = self.port',
   contentScriptWhen: 'start',
   onAttach: function onAttach(worker) {
-    worker.port.on('<=', function onMessage({ '@': address, method, params }) {
-      fs[method].apply(null, params.concat([function(error, content) {
+    worker.port.on('<=', function onMessage(message) {
+      fs[message.method].apply(fs, message.params.concat([function(error) {
           worker.port.emit('=>', {
-            '@': address,
-            params: Array.slice(arguments)
-          });
+            '@': message['@'],
+            params: [errorToJSON(error)].concat(Array.slice(arguments, 1))
+          })
       }]))
-    });
+    })
   }
 })
 
@@ -31,7 +41,8 @@ var editProtocolHandler = protocol.Handler({
   // absolute URI and returned content or content under returned URI will be
   // displayed to a user.
   onRequest: function(request, response) {
-    let { uri, referer } = request
+    var uri = request.uri
+    var referer = request.referer
     if (0 === uri.indexOf('edit:')) {
       response.content = data.load('index.html')
       response.contentType = 'text/html'
@@ -42,3 +53,5 @@ var editProtocolHandler = protocol.Handler({
   }
 })
 editProtocolHandler.listen({ scheme: PROTOCOL })
+
+});
