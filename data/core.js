7531f285da31f416eaa877aebef4f619d1d08238
Irakli Gozalishvili <rfobic@gmail.com>
2012-02-13 19:10:23 -0800

    Put together core part of editor that bootstraps everything.

---
+++ data/core.js
@@ -0,0 +1,20 @@
+/* vim:set ts=2 sw=2 sts=2 expandtab */
+/*jshint asi: true es5: true node: true browser: true devel: true */
+define(function(require, exports, module) {
+  'use strict';
+
+  var hub = require('plugin-hub/core')
+  var gcli = require('gcli-plug/core')
+  var ace = require('ace-plug/core')
+  var vice = require('vice/core')
+  var modes = require('support/mode-manager')
+
+  var env = window.env = {}
+
+  hub.plug(env, hub)
+  hub.plug(env, gcli)
+  hub.plug(env, ace)
+  hub.plug(env, vice)
+  hub.plug(env, modes)
+
+});
