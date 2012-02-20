13adc146a39c5106b62e092279319d4023df39ad
Irakli Gozalishvili <rfobic@gmail.com>
2012-02-20 03:02:11 -0800

    Replace boot and launcher with just a core module that wires all plugins together.

--- data/core.js
+++ data/core.js
@@ -8,13 +8,16 @@ define(function(require, exports, module) {
   var ace = require('ace-plug/core')
   var vice = require('vice/core')
   var modes = require('support/mode-manager')
+  var commands = require('support/command-manager')
+  var fs = require('support/fs')
 
   var env = window.env = {}
 
   hub.plug(env, hub)
   hub.plug(env, gcli)
   hub.plug(env, ace)
+  hub.plug(env, commands)
   hub.plug(env, vice)
   hub.plug(env, modes)
-
+  hub.plug(env, fs)
 });
