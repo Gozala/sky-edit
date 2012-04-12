9c635cce93847c1336dda9e50bf3c81f74df8ee1
Irakli Gozalishvili <rfobic@gmail.com>
2012-04-11 17:11:23 -0700

    Switch to newer protocol implementation.

--- raw.github.com/Gozala/jetpack-protocol/v0.2.0/index.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('../../../../@modules/raw.github.com/Gozala/jetpack-protocol/v0.2.0/index');
---
+++ raw.github.com/Gozala/jetpack-protocol/v0.3.0/index.js
@@ -0,0 +1,1 @@
+module.exports = require('../../../../@modules/raw.github.com/Gozala/jetpack-protocol/v0.3.0/index');
